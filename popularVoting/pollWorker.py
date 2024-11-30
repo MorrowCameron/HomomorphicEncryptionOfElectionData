cfe6bd94024d71b54b992b214181ee4651f09752
Michael Slu <117062577+mxsluu@users.noreply.github.com>
2024-11-30 09:46:40 -0800

    Fixed some comments in pollWorker.py

--- popularVoting/pollWorker.py
+++ popularVoting/pollWorker.py
@@ -8,12 +8,13 @@ tally = ts.ckks_vector(context, [0])
 current = "0"
 
 while True:
-    # Try to load Democrat vote
+    # Try to load current encrypted voter
     encrypted_voter_proto = utils.read_data(f"./outputs/encryptedVotes/voter" + current)
     if encrypted_voter_proto == -1:
         break
     encrypted_voter = ts.lazy_ckks_vector_from(encrypted_voter_proto)
     encrypted_voter.link_context(context)  # Link context to the encrypted data
+    # Aggregate votes without decrypting
     tally += encrypted_voter
     current = str(int(current) + 1)
 
